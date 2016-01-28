367e86f3405ecfe2da38361b23805ac0d48db0ca
Wolfgang <Andris>
2016-01-27 18:39:24 -0700

    Filled out the Formula constructor and the Evaluate method. Also created exhaustive tests for both.

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -1,6 +1,7 @@
 ﻿// Skeleton written by Joe Zachary for CS 3500, January 2015
 // Revised by Joe Zachary, January 2016
 // JLZ Repaired pair of mistakes, January 23, 2016
+// Finished by Wolfgang Andris, January 27, 2016
 
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,7 @@ namespace Formulas
     /// </summary>
     public class Formula
     {
+        public string[] tokens;
         /// <summary>
         /// Creates a Formula from a string that consists of a standard infix expression composed
         /// from non-negative floating-point numbers (using C#-like syntax for double/int literals), 
@@ -41,7 +43,109 @@ namespace Formulas
         /// </summary>
         public Formula(String formula)
         {
+            // Define the possible patterns for tokens
+            String lpPattern = @"^\($";
+            String rpPattern = @"^\)$";
+            String opPattern = @"^[\+\-*/]$";
+            String varPattern = @"^[a-zA-Z][0-9a-zA-Z]*$";
+            String doublePattern = @"^(?:\d+\.\d*|\d*\.\d+|\d+)(?:e[\+-]?\d+)?$";
+
+            // Keep track of number of left and right paren
+            int nOpenParen = 0;
+            int nCloseParen = 0;
+
+            // Empty formula is invalid
+            if (formula.Length == 0)
+            {
+                throw new FormulaFormatException("The formula cannot be empty");
+            }
+
+            // Split the input string into tokens and store them in an array
+            var tokensList = new List<string>(GetTokens(formula));
+            var tokensArray = tokensList.ToArray();
+
+            // Check if the formula contains an invalid token
+            for (int i = 0; i < tokensArray.Length - 1; i++)
+            {
+                if (!Regex.IsMatch(tokensArray[i], lpPattern) && !Regex.IsMatch(tokensArray[i], rpPattern) && !Regex.IsMatch(tokensArray[i], varPattern) &&
+                   !Regex.IsMatch(tokensArray[i], doublePattern) && !Regex.IsMatch(tokensArray[i], opPattern))
+                {
+                    throw new FormulaFormatException("Token number " + i + " is an invalid token");
+                }
+            }
+
+            // Check if first token is left paren, variable or double
+            if (!Regex.IsMatch(tokensArray[0], lpPattern) && !Regex.IsMatch(tokensArray[0], varPattern) && !Regex.IsMatch(tokensArray[0], doublePattern))
+            {
+                throw new FormulaFormatException("First token has to be a number, a variable or an opening parenthesis");
+            }
+
+            // Check if last token is right paren, variable or double
+            if (!Regex.IsMatch(tokensArray[tokensArray.Length - 1], rpPattern) && !Regex.IsMatch(tokensArray[tokensArray.Length - 1], varPattern) &&
+                !Regex.IsMatch(tokensArray[tokensArray.Length - 1], doublePattern))
+            {
+                throw new FormulaFormatException("Last token has to be a number, a variable or an opening parenthesis");
+            }
+
+            // Loop throgh the whole formula to check if its valid
+            for (int i = 0; i < tokensArray.Length - 1; i++)
+            {   
+                // Increase counter for parenthesis when one is seen            
+                if (Regex.IsMatch(tokensArray[i], lpPattern))
+                {
+                    nOpenParen++;
+                }
+                else if (Regex.IsMatch(tokensArray[i], rpPattern))
+                {
+                    nCloseParen++;
+
+                    // If at some point, there are more right than left parenthesis, throw an exception
+                    if (nCloseParen > nOpenParen)
+                    {
+                        throw new FormulaFormatException("There cant be more closing than opening parenthesis at any time");
+                    }
+                }
+
+                // If token is left paren or operator and next token is not a double, a variable or a left paren, throw an exception
+                if (Regex.IsMatch(tokensArray[i], lpPattern) || Regex.IsMatch(tokensArray[i], opPattern))
+                {
+                    if (!Regex.IsMatch(tokensArray[i + 1], doublePattern) && !Regex.IsMatch(tokensArray[i + 1], varPattern) &&
+                       !Regex.IsMatch(tokensArray[i + 1], lpPattern))
+                    {
+                        throw new FormulaFormatException("Opening parenthesis and operator can only be succeeded by a number," +
+                            " a variable or another opening parenthesis");
+                    }
+                }
+
+                // If token is double, variable or right paren and next token is not an operator or  a right paren, throw exception.
+                else if (Regex.IsMatch(tokensArray[i], doublePattern) || Regex.IsMatch(tokensArray[i], varPattern) ||
+                    Regex.IsMatch(tokensArray[i], rpPattern))
+                {
+                    if(!Regex.IsMatch(tokensArray[i + 1], opPattern) && !Regex.IsMatch(tokensArray[i + 1], rpPattern))
+                    {
+                        throw new FormulaFormatException("Numbers, variables and closing parenthesis can only be succeeded by" +
+                            " an operator or another closing parenthesis");
+                    }
+                }
+            }
+
+            // Check if last token is right paren, because loop only continued until next to last token
+            if (tokensArray[tokensArray.Length - 1] == ")")
+            {
+                nCloseParen++;
+            }
+
+            // If total number of left and right parentheses is not the same, throw exception
+            if (nCloseParen != nOpenParen)
+            {
+                throw new FormulaFormatException("In total, the formula must contain the same amount of opening parenthesis" +
+                    " and closing parenthesis");
+            }
+
+            // If everything is fine, store the array of tokens in the created object
+            tokens = tokensArray;
         }
+
         /// <summary>
         /// Evaluates this Formula, using the Lookup delegate to determine the values of variables.  (The
         /// delegate takes a variable name as a parameter and returns its value (if it has one) or throws
@@ -53,7 +157,221 @@ namespace Formulas
         /// </summary>
         public double Evaluate(Lookup lookup)
         {
-            return 0;
+            // Create two stacks, one containing the values, one containing the operators
+            var valueStack = new Stack<String>();
+            var operatorStack = new Stack<String>();
+
+            // Define patterns for variable and double tokens
+            String varPattern = @"^[a-zA-Z][0-9a-zA-Z]*$";
+            String doublePattern = @"^(?:\d+\.\d*|\d*\.\d+|\d+)(?:e[\+-]?\d+)?$";
+
+            // Loop through tokens to evaluate the expression
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                // Case token is a double
+                if (Regex.IsMatch(tokens[i], doublePattern))
+                {
+                    // If a multiplication operator is on top of the operator stack, do the mulitplication
+                    if (operatorStack.Count != 0 && operatorStack.Peek() == "*")
+                    {
+                        valueStack.Push(Convert.ToString(Convert.ToDouble(valueStack.Pop()) * Convert.ToDouble(tokens[i])));
+                        operatorStack.Pop();
+                    }
+                    // If a division operator is on top of the operator stack, do the division
+                    else if (operatorStack.Count != 0 && operatorStack.Peek() == "/")
+                    {
+                        // Check if its a division by zero
+                        if (Convert.ToDouble(tokens[i]) == 0)
+                        {
+                            throw new FormulaEvaluationException("Division by zero is impossible");
+                        }
+                        else
+                        {
+                            valueStack.Push(Convert.ToString(Convert.ToDouble(valueStack.Pop()) / Convert.ToDouble(tokens[i])));
+                            operatorStack.Pop();
+                        }
+                    }
+
+                    // If another token is on top of the operator stack or its empty, push double
+                    else
+                    {
+                        valueStack.Push(tokens[i]);
+                    }
+                }
+
+                // Case token is a variable
+                else if (Regex.IsMatch(tokens[i], varPattern))
+                {
+
+                    // Double to store the value of the variable
+                    double value = 0;
+
+                    // Look up the value of the variable. If it is undefined, throw an exception
+                    try
+                    {
+                        value = lookup(tokens[i]);
+                    }
+                    catch (UndefinedVariableException e)
+                    {
+                        throw new FormulaEvaluationException("An undefined variable was used in the formula");
+                    }
+
+                    // Check if operator stack is zero
+                    if (operatorStack.Count != 0)
+                    {
+                        // If a multiplication operator is on top of the stack, do the multiplication
+                        if (operatorStack.Peek() == "*")
+                        {
+                            valueStack.Push(Convert.ToString(Convert.ToDouble(valueStack.Pop()) * value));
+                            operatorStack.Pop();
+                        }
+
+                        // If a division operator is on top of the stack, do the division
+                        else if (operatorStack.Peek() == "/")
+                        {
+                            // If its a division by zero, throw an exception
+                            if (value == 0)
+                            {
+                                throw new FormulaEvaluationException("Division by zero is impossible");
+                            }
+                            else
+                            {
+                                valueStack.Push(Convert.ToString(Convert.ToDouble(valueStack.Pop()) / value));
+                                operatorStack.Pop();
+                            }
+                        }
+
+                        // If another token is on top of the operator stack, push the value of the variable
+                        else
+                        {
+                            valueStack.Push(Convert.ToString(value));
+                        }
+                    }
+
+                    // If the operator stack is empty, push the value of the variable
+                    else
+                    {
+                        valueStack.Push(Convert.ToString(value));
+                    }
+                }
+
+                // Case token is a + or a -
+                else if (tokens[i] == "+" || tokens[i] == "-")
+                {
+                    // Check if operator stack is empty
+                    if (operatorStack.Count != 0)
+                    {
+                        // If there is another plus or minus on top of the operator stack, apply it to
+                        // the two values on top of the value stack
+                        if (operatorStack.Peek() == "+" || operatorStack.Peek() == "-")
+                        {
+                            double secondOperand = Convert.ToDouble(valueStack.Pop());
+                            double firstOperand = Convert.ToDouble(valueStack.Pop());
+
+                            if (operatorStack.Peek() == "+")
+                            {
+                                valueStack.Push(Convert.ToString(firstOperand + secondOperand));
+                            }
+                            if (operatorStack.Peek() == "-")
+                            {
+                                valueStack.Push(Convert.ToString(firstOperand - secondOperand));
+                            }
+                            operatorStack.Pop();
+                        }
+                    }
+
+                    // If operator stack is empty, push the plus or minus
+                    operatorStack.Push(tokens[i]);
+                }
+
+                // Case token is a multiplication sign, a division sign or a left paren, just push it
+                else if (tokens[i] == "*" || tokens[i] == "/" || tokens[i] == "(")
+                {
+                    operatorStack.Push(tokens[i]);
+                }
+
+                // Case token is a right paren
+                else
+                {
+                    // If there is another plus or minus on top of the operator stack, apply it to
+                    // the two values on top of the value stack
+                    if (operatorStack.Peek() == "+" || operatorStack.Peek() == "-")
+                    {
+                        double secondOperand = Convert.ToDouble(valueStack.Pop());
+                        double firstOperand = Convert.ToDouble(valueStack.Pop());
+
+                        if (operatorStack.Peek() == "+")
+                        {
+                            valueStack.Push(Convert.ToString(firstOperand + secondOperand));
+                        }
+                        if (operatorStack.Peek() == "-")
+                        {
+                            valueStack.Push(Convert.ToString(firstOperand - secondOperand));
+                        }
+                        operatorStack.Pop();
+                    }
+
+                    // Now a left paren is on top of the operator stack, push it
+                    operatorStack.Pop();
+
+                    // Check if operator stack is empty
+                    if (operatorStack.Count != 0)
+                    {
+                        // If a multiplication sign is on top of the operator stack, apply
+                        // it to the two values on top of the stack
+                        if (operatorStack.Peek() == "*")
+                        {
+                            Double secondOperand = Convert.ToDouble(valueStack.Pop());
+                            Double firstOperand = Convert.ToDouble(valueStack.Pop());
+
+                            valueStack.Push(Convert.ToString(firstOperand * secondOperand));
+                            operatorStack.Pop();
+                        }
+
+                        // If a division sign is on top of the operator stack, apply
+                        // it to the two values on top of the stack
+                        else if (operatorStack.Peek() == "/")
+                        {
+                            // If its a division by zero, throw an exception
+                            if (Convert.ToDouble(valueStack.Peek()) == 0)
+                            {
+                                throw new FormulaEvaluationException("Division by zero is impossible");
+                            }
+                            else
+                            {
+                                Double secondOperand = Convert.ToDouble(valueStack.Pop());
+                                Double firstOperand = Convert.ToDouble(valueStack.Pop());
+
+                                valueStack.Push(Convert.ToString(firstOperand / secondOperand));
+                                operatorStack.Pop();
+                            }
+                        }
+                    }
+                }
+            }
+
+            // After every token is processed, if the operator stack is empty, the value on the 
+            // value stack is the result of the formula
+            if (operatorStack.Count == 0)
+            {
+                return Convert.ToDouble(valueStack.Pop());
+            }
+
+            // If there is an operator left on the operator stack, apply it to the two values, that
+            // are left on the value stack. That result is the result of the formula
+            else
+            {
+                double secondOperand = Convert.ToDouble(valueStack.Pop());
+                double firstOperand = Convert.ToDouble(valueStack.Pop());
+                if (operatorStack.Peek() == "+")
+                {
+                    return firstOperand + secondOperand;
+                }
+                else
+                {
+                    return firstOperand - secondOperand;
+                }
+            }
         }
 
         /// <summary>
@@ -139,3 +457,4 @@ namespace Formulas
         }
     }
 }
+
--- Spreadsheet/FormulaSimpleTests/FormulaSimpleTests.cs
+++ Spreadsheet/FormulaSimpleTests/FormulaSimpleTests.cs
@@ -9,10 +9,7 @@ using Formulas;
 namespace FormulaTestCases
 {
     /// <summary>
-    /// These test cases are in no sense comprehensive!  They are intended to show you how
-    /// client code can make use of the Formula class, and to show you how to create your
-    /// own (which we strongly recommend).  To run them, pull down the Test menu and do
-    /// Run > All Tests.
+    /// A class containing test cases for the Formula class
     /// </summary>
     [TestClass]
     public class UnitTests
@@ -49,6 +46,106 @@ namespace FormulaTestCases
         }
 
         /// <summary>
+        /// Another syntax error, more left than right paren.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct4()
+        {
+            Formula f = new Formula("(3+5)*7)+(/2");
+        }
+
+        /// <summary>
+        /// Another syntax error, invalid token.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct5()
+        {
+            Formula f = new Formula("(3+#)*4");
+        }
+
+        /// <summary>
+        /// Another syntax error, more right than left paren.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct6()
+        {
+            Formula f = new Formula("(3+5)/12e4*(7+3))");
+        }
+
+        /// <summary>
+        /// Another syntax error, invalid first token.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct7()
+        {
+            Formula f = new Formula("*(3+4)");
+        }
+
+        /// <summary>
+        /// Another syntax error, invalid last token.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct8()
+        {
+            Formula f = new Formula("(7+2)/");
+        }
+
+        /// <summary>
+        /// Another syntax error, operator after opening paren.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct9()
+        {
+            Formula f = new Formula("3*(/2)");
+        }
+
+        /// <summary>
+        /// Another syntax error, operator after operator.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct10()
+        {
+            Formula f = new Formula("4/*");
+        }
+
+        /// <summary>
+        /// Another syntax error, double after closing paren.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct11()
+        {
+            Formula f = new Formula("4*(3+5)6");
+        }
+
+        /// <summary>
+        /// Another syntax error, opening paren after variable.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct12()
+        {
+            Formula f = new Formula("z(3+2)");
+        }
+
+        /// <summary>
+        /// Another syntax error, empty formula.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Construct13()
+        {
+            Formula f = new Formula("");
+        }
+
+        /// <summary>
         /// Makes sure that "2+3" evaluates to 5.  Since the Formula
         /// contains no variables, the delegate passed in as the
         /// parameter doesn't matter.  We are passing in one that
@@ -77,7 +174,7 @@ namespace FormulaTestCases
         /// <summary>
         /// Here, the delegate passed to Evaluate always throws a
         /// FormulaEvaluationException (meaning that no variables have
-        /// values).  The test case checks that the result of
+        /// values). The test case checks that the result of
         /// evaluating the Formula is a FormulaEvaluationException.
         /// </summary>
         [TestMethod]
@@ -109,6 +206,168 @@ namespace FormulaTestCases
             Assert.AreEqual(f.Evaluate(Lookup4), 20.0, 1e-6);
         }
 
+        /// <summary>
+        /// Testing UndefinedVariableException, which leads to a FormulaEvaluationException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate6 ()
+        {
+            Formula f = new Formula("(x + y) * q * 1.0");
+            Assert.AreEqual(f.Evaluate(Lookup4), 20.0, 1e-6);
+        }
+
+        /// <summary>
+        /// Double after multiplication operator.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate7()
+        {
+            Formula f = new Formula("9*4");
+            Assert.AreEqual(f.Evaluate(v => 0), 36.0, 1e-6);
+        }
+        /// <summary>
+        /// Double that gets pushed and Double after multiplication operator.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate8()
+        {
+            Formula f = new Formula("(3 + 12) * 9");
+            Assert.AreEqual(f.Evaluate(v => 0), 135.0, 1e-6);
+        }
+        /// <summary>
+        /// Variable after multiplication.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate9()
+        {
+            Formula f = new Formula("x1 * x2");
+            Assert.AreEqual(f.Evaluate(v => 5), 25.0, 1e-6);
+        }
+        /// <summary>
+        /// Variable that gets pushed and variable after multiplication operator.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate10()
+        {
+            Formula f = new Formula("(4 - x1)*x2");
+            Assert.AreEqual(f.Evaluate(v => 3), 3.0, 1e-6);
+        }
+        /// <summary>
+        /// Double divided by zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate11()
+        {
+            Formula f = new Formula("7/0");
+            Assert.AreEqual(f.Evaluate(v => 0), 20.0, 1e-6);
+        }
+        /// <summary>
+        /// Variable divided by zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate12()
+        {
+            Formula f = new Formula("x1/0");
+            Assert.AreEqual(f.Evaluate(v => 0), 20.0, 1e-6);
+        }
+        /// <summary>
+        /// Only double and parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate13()
+        {
+            Formula f = new Formula("(3)");
+            Assert.AreEqual(f.Evaluate(Lookup4), 3.0, 1e-6);
+        }
+        /// <summary>
+        /// Only variable and parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate14()
+        {
+            Formula f = new Formula("(x1)");
+            Assert.AreEqual(f.Evaluate(v => 10.55), 10.55, 1e-6);
+        }
+        /// <summary>
+        /// Addition with + on operator stack.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate15()
+        {
+            Formula f = new Formula("3 + 1e-3 + 14");
+            Assert.AreEqual(f.Evaluate(Lookup4), 17.001, 1e-6);
+        }
+        /// <summary>
+        /// Subtraction with + on operator stack.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate16()
+        {
+            Formula f = new Formula("4.1 + 2.3e5 - 6");
+            Assert.AreEqual(f.Evaluate(Lookup4), 229998.1, 1e-6);
+        }
+        /// <summary>
+        /// Closing parenthesis with a division operator on top of stack.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate17()
+        {
+            Formula f = new Formula("(7 + 3)/(1 + 1)");
+            Assert.AreEqual(f.Evaluate(Lookup4), 5.0, 1e-6);
+        }
+        /// <summary>
+        /// No empty operator stack at the end
+        /// </summary>
+        [TestMethod]
+        public void Evaluate18()
+        {
+            Formula f = new Formula("(4.3) - (x1-3)");
+            Assert.AreEqual(f.Evaluate(v => 3), 4.3, 1e-6);
+        }
+        /// <summary>
+        /// Just a long calculation.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate19()
+        {
+            Formula f = new Formula("3*(2e5-7)/100*x1-(x2+6)+2.2e3/10");
+            Assert.AreEqual(f.Evaluate(v => 10), 60201.9, 1e-6);
+        }
+
+        /// <summary>
+        /// Division by a variable which is zero.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate20()
+        {
+            Formula f = new Formula("8/x1");
+            Assert.AreEqual(f.Evaluate(v => 0), 60201.9, 1e-6);
+        }
+
+        /// <summary>
+        /// Division by zero in parentheses.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Evaluate21()
+        {
+            Formula f = new Formula("5/(3-3)");
+            Assert.AreEqual(f.Evaluate(v => 10), 60201.9, 1e-6);
+        }
+
+        /// <summary>
+        /// A subtraction that leads to a negative number.
+        /// </summary>
+        [TestMethod]
+        public void Evaluate22()
+        {
+            Formula f = new Formula("2 + 3*(3 - 5)");
+            Assert.AreEqual(f.Evaluate(v => 0), -4, 1e-6);
+        }
         /// <summary>
         /// A Lookup method that maps x to 4.0, y to 6.0, and z to 8.0.
         /// All other variables result in an UndefinedVariableException.
